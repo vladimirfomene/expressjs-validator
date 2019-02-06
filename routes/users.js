957edd9dc8ac2a6d84b30b7b460ee4a812f172e6
Vladimir Fomene <vladimirfomene@gmail.com>
2019-02-06 15:00:21 +0100

    remove fields from fields on the homepage

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,9 @@ const {check, validationResult} = require('express-validator/check');
 const router = express.Router();
 
 router.post('/', [
-    check('firstName', 'First name must have more than 2 characters').exists().isLength({min: 2}),
-    check('lastName', 'Last name must have more than 2 characters').exists().isLength({min: 2}),
+    check('name', 'Name must have more than 5 characters').exists().isLength({min: 5}),
     check('classYear', 'Class Year should be a number').exists().isInt(),
     check('email', 'Your email is not valid').exists().isEmail(),
-    check('phoneNumber', 'Your phone number must be at least 8 digits').exists().isLength({min: 8}),
     check('password', 'Your password must be at least 5 characters').exists().isLength({min: 5}),
     check('confirmPassword', 'Passwords do not match').custom((value, {req}) => (value === req.body.password)),
     check('isAdmin').optional().isIn(['on', 'off'])
