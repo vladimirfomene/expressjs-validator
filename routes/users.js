f3342b328440b2c5242b43e9826414b41d5d42e6
Vladimir Fomene <vladimirfomene@gmail.com>
2019-02-16 08:26:43 +0100

    replace exists() with not().isEmpty()

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const {check, validationResult} = require('express-validator/check');
-const { sanitizeBody } = require('express-validator/filter');
 const router = express.Router();
 
+
+/* GET users page. */
+router.get('/', function (req, res) {
+  res.send();
+});
+
+/* receive user input through POST */
 router.post('/', [
     check('name').exists().isLength({min: 5}).trim().escape().withMessage('Name must have more than 5 characters'),
-    check('classYear', 'Class Year should be a number').exists().isInt(),
-    check('weekday', 'Choose a weekday').trim().optional().not().isIn(['sunday', 'saturday']),
-    check('email', 'Your email is not valid').exists().isEmail().normalizeEmail(),
-    check('password', 'Your password must be at least 5 characters').exists().isLength({min: 5}),
+    check('classYear', 'Class Year should be a number').not().isEmpty().isInt(),
+    check('weekday', 'Choose a weekday').optional().not().isIn(['sunday', 'saturday']),
+    check('email', 'Your email is not valid').not().isEmpty().isEmail().normalizeEmail(),
+    check('password', 'Your password must be at least 5 characters').not().isEmpty().isLength({min: 5}),
     check('confirmPassword', 'Passwords do not match').custom((value, {req}) => (value === req.body.password))
   ],
   function (req, res) {
